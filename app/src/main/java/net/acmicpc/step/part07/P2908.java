e677b3b056b6d698ff803741fcaecd04ffebbb85
aerhergag0 <aerhergag@naver.com>
2021-07-24 22:53:38 +0900

    0724

--- app/src/main/java/net/acmicpc/step/part07/P2908.java
+++ app/src/main/java/net/acmicpc/step/part07/P2908.java
@@ -1,5 +1,23 @@
 package net.acmicpc.step.part07;
 
-public class P2908 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
+// 첫줄에 두 수 A,B가 주어진다. 두 수는 같지않은 세자리수 이고 0이 포함되어 있지 않다.
+// 두 수를 역순으로 한뒤 큰 수를 출력
+public class P2908 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int A = Integer.parseInt(st.nextToken());
+    int B = Integer.parseInt(st.nextToken());
+
+
+  }
 }
